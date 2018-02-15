e20c993397972eb1a739fa6bb217f273a90eb562
Julianne Quinn <julianneq@users.noreply.github.com>
2018-02-15 16:05:37 -0500

    Create makeAllFigures.py

---
+++ PaperFigures/makeAllFigures.py
@@ -0,0 +1,21 @@
+from makeFigure1 import makeFigure1
+from makeFigure2 import makeFigure2
+from makeFigure3 import makeFigure3
+from makeFigure4 import makeFigure4
+from makeFigure5 import makeFigure5
+from makeFigure6 import makeFigure6
+from makeFigure7 import makeFigure7
+from makeFigure8 import makeFigure8
+from makeFigureS1 import makeFigureS1
+from makeFigureS2 import makeFigureS2
+
+makeFigure1()
+makeFigure2()
+makeFigure3()
+makeFigure4()
+makeFigure5()
+makeFigure6()
+makeFigure7()
+makeFigure8()
+makeFigureS1()
+makeFigureS2()
