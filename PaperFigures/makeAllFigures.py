337f10dba109f4bd790f0263a875a5a446123b58
Julianne Quinn <julianneq@users.noreply.github.com>
2018-02-15 16:30:21 -0500

    Update makeAllFigures.py

--- PaperFigures/makeAllFigures.py
+++ PaperFigures/makeAllFigures.py
@@ -4,18 +4,20 @@ from makeFigure3 import makeFigure3
 from makeFigure4 import makeFigure4
 from makeFigure5 import makeFigure5
 from makeFigure6 import makeFigure6
-from makeFigure7 import makeFigure7
-from makeFigure8 import makeFigure8
-from makeFigureS1 import makeFigureS1
-from makeFigureS2 import makeFigureS2
+from plotShadedDensity import plotShadedDensity
+from plotSimulations import plotSimulations
 
-makeFigure1()
-makeFigure2()
-makeFigure3()
-makeFigure4()
-makeFigure5()
-makeFigure6()
-makeFigure7()
-makeFigure8()
-makeFigureS1()
-makeFigureS2()
+def makeAllFigures():
+  
+  makeFigure1()
+  makeFigure2()
+  makeFigure3()
+  makeFigure4()
+  makeFigure5()
+  makeFigure6()
+  plotSimulations() # makes Figures 7 and 8
+  plotShadedDensity() # makes Figures S1 and S2
+  
+  return None
+
+makeAllFigures()
